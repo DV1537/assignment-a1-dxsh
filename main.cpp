64f6b58fc091c2a23855ac21675813e304ccd37b
Darwin Shameran <dxsh@riseup.net>
2018-11-08 00:40:56 +0100

    refactor and integrate realloc() into main(); implement remaining logic

--- main.cpp
+++ main.cpp
@@ -1,56 +1,63 @@
 #include <iostream>
 #include <fstream>
 
-void realloc(int[], int);
+void realloc(size_t[], size_t);
 
 /**
- * The program reads numbers from a file and prints the sum of them
+ * The program reads numbers from a file and prsize_ts the sum of them
  * Example: main.exe ./tests/input.in
  * @param  argc [description]
  * @param  argv [description]
  * @return      [description]
  */
-int main(int argc, const char * argv[]) {
-    int a = 0;
-    int sum = 0;
-    int arraySize = 1;
-    int numbersAdded = 0;
-    int* numbers = new int[arraySize];
+int main(int argc, const char* argv[]) {
+  size_t a = 0;
+  size_t sum = 0;
+  size_t avg = 0;
+  size_t arraySize = 1;
+  size_t numbersAdded = 0;
+  size_t* numbers = new size_t[arraySize];
 
-    std::ifstream myReadFile(argv[1]);
+  std::ifstream myReadFile(argv[1]);
 
-    while (myReadFile >> a) {
-        sum += a;
-        numbersAdded++;
+  if (!myReadFile.is_open()) return 2;
 
-        /* If array is full and not at EOF, expand by 1 */
-        if (numbersAdded == arraySize) {
-          realloc(numbers, arraySize);
-          arraySize++;
-        }
+  while (myReadFile >> a) {
+    /* Expand array if numbers added is greater or equals size of array */ 
+    if (numbersAdded >= arraySize) {
+      size_t *tmp = new size_t[arraySize + 1];
+
+      for (size_t i = 0; i < arraySize; i++) {
+        tmp[i] = numbers[i];
+      }
+
+      delete[] numbers;
+      numbers = tmp;
+      tmp = nullptr;
+
+      arraySize += 1;
     }
 
-    myReadFile.close();
-
-    std::cout << sum << "\n";
-
-    return 0;
-}
-
-/**
- * Expand the passed dynamically allocated array by arraySize + 1
- */
-void realloc(int numbers[], int arraySize) {
-  int *tmp = new int[arraySize + 1];
-
-  for (int i = 0; i < (arraySize - 1); i++) {
-    tmp[i] = numbers[i];
+    numbers[numbersAdded] = a;
+    sum += a;
+    numbersAdded++;
   }
 
-  delete[] numbers;
+  myReadFile.close();
 
-  numbers = tmp;
-  tmp = nullptr;
+  avg = sum / arraySize;
+
+  std::cout << "Input: "; 
+  for (size_t i = 0; i < arraySize; i++) {
+    std::cout << numbers[i] << '\t';
+  }
+
+  std::cout << "\tOutput: ";
+  for (size_t i = 0; i < arraySize; i++) {
+    (numbers[i] > avg) ? std::cout << numbers[i] << '\t': std::cout << "";
+  }
+
+  return 0;
 }
 
 /* vim: set ts=2 sw=2 tw=79 ft=cpp et :*/
