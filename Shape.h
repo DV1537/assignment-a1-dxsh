c260548b8dbdaa7defc589ee58d3fa0788bbb2cc
Darwin Shameran <dxsh@riseup.net>
2018-11-08 01:10:14 +0100

    + Shape.h

---
+++ Shape.h
@@ -0,0 +1,21 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <string>
+
+class Shape {
+ private:
+   std::string shape;
+ 
+ public:
+   Shape();
+   Shape(std::string shape) : shape{shape} {}  /* ? */
+   virtual std::string getType() const = 0;
+   virtual int area() const = 0;
+   virtual int positon() const = 0;
+   virtual bool isConvex() const = 0;
+   virtual int distance(Shape &s) const = 0;
+};
+
+#endif
+/* vim: set ts=2 sw=2 tw=79 ft=cpp et :*/
