235cca1280534aa6289b3d8a0fbdce247bb188e1
Darwin Shameran <dxsh@riseup.net>
2018-11-08 19:11:48 +0100

    + Point.cpp

---
+++ Point.cpp
@@ -0,0 +1,22 @@
+#include "Point.h"
+
+std::string Point::getType() const {
+
+}
+
+int Point::area() const {
+
+}
+
+int Point::position() const {
+}
+
+bool Point::isConvex() const {
+
+}
+
+int Point::distance(Shape& s) const {
+
+}
+
+/* vim: set ts=2 sw=2 tw=79 ft=cpp et :*/
